f663b0c9dd854ca5e00fb01e1b215325a40d047b
john-shika <skulluglify@outlook.com>
2024-10-10 20:41:39 +0700

    [feat] update files

--- scripts/js-bundle-setup.ps1
+++ scripts/js-bundle-setup.ps1
@@ -5,14 +5,14 @@ Set-Location $currentWorkDir -ErrorAction Stop
 Set-Location ..
 
 $url = "https://cdn.jsdelivr.net/npm/@scalar/api-reference"
-$outputFilePath = "public/js/scalar.api-reference.js"
+$outputFile = "public/js/scalar.api-reference.js"
 
-$directory = [System.IO.Path]::GetDirectoryName($outputFilePath)
+$directory = [System.IO.Path]::GetDirectoryName($outputFile)
 if (-not (Test-Path -Path $directory)) {
     New-Item -ItemType Directory -Path $directory -Force
 }
 
-Invoke-WebRequest -Uri $url -OutFile $outputFilePath
+Invoke-WebRequest -Uri $url -OutFile $outputFile
 
 Set-Location $currentWorkDir
 Set-Location ..
